a58f51165a7094d3069f6fe88af40355fb6a6d41
gmazur92 <gmazur92@gmail.com>
2021-05-18 22:19:20 +0300

    refactor: add config file, migrate from commonjs to modules

---
+++ src/common/config.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+const __dirname = path.resolve();
+
+dotenv.config({
+  path: path.join(__dirname, '../../.env'),
+});
+
+export default {
+  DB_NAME: process.env.DB_NAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_USER: process.env.DB_USER,
+  PORT: process.env.PORT,
+  SECRET: process.env.SECRET_KEY,
+};
