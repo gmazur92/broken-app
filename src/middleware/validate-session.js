8a098898e26b60d519e6475a82e4c5ebcc0c372c
gmazur92 <gmazur92@gmail.com>
2021-05-23 18:04:28 +0300

    refactor: remove request to db, middleware is looking to get Bearer Token

--- src/middleware/validate-session.js
+++ src/middleware/validate-session.js
@@ -1,36 +1,21 @@
 import jwt from 'jsonwebtoken';
-import User from '../resources/auth/user.model.js';
 import config from '../common/config.js';
 
 const validateSession = async(req, res, next) => {
-  const sessionToken = req.headers.authorization;
-  let decoded;
-
   if (req.method === 'OPTIONS') {
     next();   // allowing options as a method for request
     return;
   }
-  if (!sessionToken) {
-    return res.status(401).send({auth: false, message: 'No token provided.'});
-  }
-
   try {
-    decoded = jwt.verify(sessionToken, config.SECRET);
-  } catch (err) {
-    return res.status(401).send({error: 'Token expired'});
+    const sessionToken = req.headers.authorization.split(' ')[ 1 ]; //get only token without Bearer
+    if (!sessionToken) {
+      return res.status(401).json({auth: false, message: 'No token provided.'});
+    }
+    req.user = jwt.verify(sessionToken, config.SECRET);
+    next();
+  } catch (e) {
+    return res.status(401).json({error: 'Token expired'});
   }
-
-  const user = await User.findOne({where: {id: decoded.id}});
-  if (!user) {
-    res.status(401).send({error: 'not authorized'});
-  }
-  req.user = {
-    id: user.dataValues.id,
-    full_name: user.dataValues.full_name,
-    email: user.dataValues.email,
-    username: user.dataValues.username,
-  };
-  next();
 };
 
 export default validateSession;
